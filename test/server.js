6b6e731a18a01a4a620162e4c7b712b6e0a372ab
nimojs <nimo.jser@gmail.com>
2016-05-13 20:18:36 +0800

    add cookies test

--- test/server.js
+++ test/server.js
@@ -22,5 +22,17 @@ describe('server.js', function() {
                 })
             })
         })
+        it('# cookieParser', function (done) {
+            var app = mobe({autoListen: false})
+            var server = supertest(app.app)
+            app.app.get('/cookieParser/', function (req, res) {
+                res.send(req.cookies)
+            })
+            server
+               .get('/cookieParser/')
+               .set('Cookie', 'name=mobe')
+               .expect('{"name":"mobe"}')
+               .expect(200, done)
+        })
     })
 })
