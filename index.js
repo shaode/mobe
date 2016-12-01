512d34e87718e33e34a782fab12618c0f9718c3a
nimojs <nimo.jser@gmail.com>
2016-12-01 10:44:37 +0800

    babel

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /*
-Copyright(c) 2013-2016 Nimo Chu
+    Copyright(c) 2013-2017 Nimo Chu
+    http://mose.nimojs.com
 */
+require('babel-polyfill')
 require('babel-core/register')
 module.exports = require('./lib/')
