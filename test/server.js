351e170ee9ed66c482baf950dcd2cab0102b45bc
nimojs <nimo.jser@gmail.com>
2016-12-01 10:59:16 +0800

    hash port

--- test/server.js
+++ test/server.js
@@ -4,15 +4,15 @@ var chai = require('chai')
 var expect = chai.expect
 var should = chai.should
 var assert = chai.assert
-var mobe = require('../index')
+var mose = require('../index')
 var portfinder = require('../lib/vendor/portfinder')
 
 describe('server.js', function() {
     describe('# basic server run', function () {
-        it('# listen server', function (done) {
+        it('# listen port', function (done) {
             var app
             portfinder.getPort(function (err, port) {
-                app = mobe({port: port})
+                app = mose({port: port})
                 req('http://127.0.0.1:' + app.config.port, function (error, response, body) {
                     if (error) {
                         throw error
@@ -22,38 +22,80 @@ describe('server.js', function() {
                 })
             })
         })
+        it('# listen hasport default', function (done) {
+            var app
+            var url = 'http://127.0.0.1:50918'
+            req(url, function (error, response, body) {
+                if (error) {
+                    if (error.message === 'connect ECONNREFUSED 127.0.0.1:50918') {
+                        done()
+                        return
+                    }
+                    throw error
+                }
+                app = mose()
+                req(url, function (error, response, body) {
+                    if (error) {
+                        throw error
+                    }
+                    expect(body).to.equal('Cannot GET /\n')
+                    done()
+                })
+            })
+        })
+        it('# listen hasport name:nimo', function (done) {
+            var app
+            var url = 'http://127.0.0.1:58484'
+            req(url, function (error, response, body) {
+                if (error) {
+                    if (error.message === 'connect ECONNREFUSED 127.0.0.1:58484') {
+                        done()
+                        return
+                    }
+                    throw error
+                }
+                app = mose({name: "nimo"})
+                req(url, function (error, response, body) {
+                    if (error) {
+                        throw error
+                    }
+                    expect(body).to.equal('Cannot GET /\n')
+                    done()
+                })
+            })
+        })
         it('# cookieParser', function (done) {
-            var app = mobe({autoListen: false})
+            var app = mose({autoListen: false})
             var server = supertest(app.app)
             app.app.get('/cookieParser/', function (req, res) {
                 res.send(req.cookies)
             })
             server
                .get('/cookieParser/')
-               .set('Cookie', 'name=mobe')
-               .expect('{"name":"mobe"}')
+               .set('Cookie', 'name=mose')
+               .expect('{"name":"mose"}')
                .expect(200, done)
         })
         it('# get', function (done) {
-            var app = mobe({autoListen: false})
+            var app = mose({autoListen: false})
             var server = supertest(app.app)
             app.app.get('/get/', function (req, res) {
                 res.send(req.query)
             })
             server
-               .get('/get/?name=getmobe')
-               .expect('{"name":"getmobe"}')
+               .get('/get/?name=getmose')
+               .expect('{"name":"getmose"}')
                .expect(200, done)
         })
         it('# post', function (done) {
-            var app = mobe({autoListen: false})
+            var app = mose({autoListen: false})
             var server = supertest(app.app)
             app.app.get('/post/', function (req, res) {
                 res.send(req.query)
             })
             server
-               .get('/post/?name=postmobe')
-               .expect('{"name":"postmobe"}')
+               .get('/post/?name=postmose')
+               .expect('{"name":"postmose"}')
                .expect(200, done)
         })
     })
