d6a451d7aacf5f839a287c144240f1f18df48900
nimojs <nimo.jser@gmail.com>
2016-12-04 00:01:43 +0800

    babel

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
     Copyright(c) 2013-2017 Nimo Chu
     http://mose.nimojs.com
 */
-require('babel-polyfill')
-require('babel-core/register')
-module.exports = require('./lib/')
+const mose = require('./lib/')
+module.exports = mose
